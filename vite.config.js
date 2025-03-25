3e96ea77cd701c0fc9c96fa89f01b0916b2f463c
vishantjurish <vishant.juris@gmail.com>
2025-03-25 16:17:11 +0530

    modjhsdfi

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,3 +7,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),  tailwindcss()]
 })
+=======
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
+>>>>>>> Stashed changes
