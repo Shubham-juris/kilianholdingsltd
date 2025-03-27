f9db711bb3a88f1cee8049c18555221379b1b805
mohibjuris <mohib.juris@gmail.com>
2025-03-27 10:59:33 +0530

    made new branch and update terms

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import "./App.css";
 import TopHeader from "./Components/TopHeader/TopHeader";
 import Navbar from "./Components/Navbar/Navbar";
-import Herosection from "./Components/Herosection/Herosection";
-import WelcomeSection from "./Components/WelcomeSection/WelcomeSection";
-import AboutSection from "./Components/AboutSection/AboutSection";
 import Footer from "./Components/Footer/Footer";
-import Disclaimer from "./Components/Disclaimer/Disclaimer";
-import Account_Book from "./Components/account_And_Book_Keeping/Account_Book";
-import AboutUs from "./Components/AboutUs/AboutUs";
-import ContactUs from "./Components/ContactUs/ContactUs";
 // import Sitemaintenance from './Components/Sitemaintenance/Sitemaintenance'
+// react router
+
+import AppRouter from "./Router/AppRouter";
 
 function App() {
   return (
     <>
       <TopHeader />
       <Navbar />
-      <Herosection />
-      <WelcomeSection />
-      <AboutSection />
-      <Disclaimer />
-      <Account_Book />
-      <AboutUs />
-      <ContactUs />
+
+      {/* app routes */}
+      <AppRouter />
+
       <Footer />
-      
-      {/* <Sitemaintenance/> */}
     </>
   );
 }
