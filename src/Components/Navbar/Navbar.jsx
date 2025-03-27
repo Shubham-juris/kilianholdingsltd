54252ae4c221ea6bad58ab0215ea23d36875ebfc
rishavjuris <rishav.juris@gmail.com>
2025-03-27 16:33:11 +0530

    rgtrgr

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -32,14 +32,14 @@ const Navbar = () => {
 
         {/* Sidebar Drawer */}
         <div
-          className={`fixed top-0 left-0 z-40 w-[20em] h-screen p-6 overflow-y-auto transition-transform bg-white dark:bg-gray-800 shadow-lg ${
+          className={`fixed top-0 left-0 z-40 w-[24em] h-screen p-4 overflow-y-auto transition-transform bg-white dark:bg-gray-800 ${
             isOpen ? "translate-x-0" : "-translate-x-full"
           }`}
         >
           {/* Header */}
-          <div className="flex justify-end">
+          <div className="float-right">
             <button
-              className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-600"
+              className="hover:bg-gray-200 dark:hover:bg-gray-600 "
               onClick={() => setIsOpen(false)}
             >
               ✖
@@ -47,56 +47,90 @@ const Navbar = () => {
           </div>
 
           {/* Navigation Links */}
-          <ul className="font-medium mt-6 space-y-4">
+          <ul className="font-medium" style={{ padding: "5em" }}>
             <li>
-              <Link to="/" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                HOME
+              <Link
+                to="/"
+                onClick={() => setIsOpen(false)}
+                className="flex items-center p-10 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">HOME</span>
               </Link>
             </li>
             <li>
-              <Link to="/aboutus" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                ABOUT US
+              <Link
+                to="/aboutus"
+                onClick={() => setIsOpen(false)}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">ABOUT US</span>
               </Link>
             </li>
             <li>
-              <Link to="/services" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                SERVICES
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">SERVICES</span>
+              </a>
+            </li>
+            <li>
+              <Link
+                to="accountbook"
+                onClick={() => setIsOpen(false)}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">ACCOUNTING & BOOKKEEPING</span>
               </Link>
             </li>
             <li>
-              <Link to="/accountbook" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                ACCOUNTING & BOOKKEEPING
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">STOCK MARKET</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">BOOK A CONSULTATION</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="/clintportal"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">CLINT PORTAL</span>
+              </a>
+            </li>
+            <li>
+              <Link
+                to="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">CEREERS</span>
               </Link>
             </li>
             <li>
-              <Link to="/stockmarket" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                STOCK MARKET
+              <Link
+                to="/contactus"
+                onClick={() => setIsOpen(false)}
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">CONTACT US</span>
               </Link>
             </li>
             <li>
-              <Link to="/consultation" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                BOOK A CONSULTATION
-              </Link>
-            </li>
-            <li>
-              <Link to="/clientportal" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                CLIENT PORTAL
-              </Link>
-            </li>
-            <li>
-              <Link to="/careers" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                CAREERS
-              </Link>
-            </li>
-            <li>
-              <Link to="/contactus" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                CONTACT US
-              </Link>
-            </li>
-            <li>
-              <Link to="/blogs" onClick={() => setIsOpen(false)} className="block p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                BLOGS
-              </Link>
+              <a
+                href="/Blogs"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">BLOGS</span>
+              </a>
             </li>
           </ul>
         </div>
