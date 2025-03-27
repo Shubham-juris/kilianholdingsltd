f068f29332a7cb3b76b7d0d1d4e64c5808b0adcf
Deepak98720 <deepak.jurisnexa@gmail.com>
2025-03-27 14:58:16 +0530

    hajdfhcas

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -3,11 +3,13 @@ import Home from "../pages/Home";
 import AboutUsPage from "../pages/AboutUsPage";
 import AccountBookPage from "../pages/AccountBookPage";
 import ContactUsPage from "../pages/ContactUsPage";
+import Detail from "../pages/Detail"; // Ensure this component exists
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/detail" element={<Detail />} />
       <Route path="/aboutus" element={<AboutUsPage />} />
       <Route path="/accountbook" element={<AccountBookPage />} />
       <Route path="/contactus" element={<ContactUsPage />} />
